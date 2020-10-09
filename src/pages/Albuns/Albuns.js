f37c33cf1b9aff23e905e17d5666c7360a02a65b
guvarallo <gustavo.varallo@live.com>
2020-10-09 08:52:38 -0300

    added albuns page

--- src/pages/Albuns/Albuns.js
+++ src/pages/Albuns/Albuns.js
@@ -1,5 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Row, Col, Card } from 'antd';
 
-function Album() {
-  
+import './Albuns.css';
+
+function Albuns({ albuns }) {
+  return (
+    <>
+      {albuns.length &&
+        <Row gutter={16}>
+            {albuns.map(album => {
+              return (
+                <Col span={8}>
+                  <Card
+                    hoverable
+                    cover={<img alt="example" src={album.thumb} />}
+                    style={{ width: "100%", margin: "10px" }}
+                  >
+                    <p className="title">{album.title}</p>
+                  </Card>
+                </Col>
+              )
+            })}
+        </Row>
+      }
+    </>
+  )
 }
+
+export default Albuns;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,30 +1,30 @@
 import React, { useState } from 'react';
-import { Input, Button } from 'antd';
+import { Radio, Input, Button, Spin } from 'antd';
 import { Link } from 'react-router-dom';
+
+import Albuns from '../Albuns/Albuns';
 
 import './Main.css';
 
 function Main() {
-  const [searchTerm, setSearchTerm] = useState({term: ''});
-  // const [albumResults, setAlbumResults] = useState([]);
-  const [artistResults, setArtistResults] = useState([{}]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [radioValue, setRadioValue] = useState('artist');
+  const [artists, setArtists] = useState([]);
+  const [masters, setMasters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const apiKey = 'xHkvirUsAkQKmuPtVhkNgQFyjUkQbbWFAhKwREnq';
   const url = 'https://api.discogs.com/database/search?';
 
+  function handleRadioValue(event) {
+    setRadioValue(event.target.value);
+  }
+
   function handleTermChange(event) {
-    setSearchTerm({term: event.target.value});
+    setSearchTerm(event.target.value);
   }
 
-  function handleArtistSearch() {
-    searchArtist(searchTerm.term);
-  }
-
-  // function handleAlbumSearch() {
-  //   searchAlbum(searchTerm.term);
-  // }
-
-
-  function searchArtist(term) {
+  function handleSearch(term) {
+    setIsLoading(true);
     try {
       fetch(`${url}q=${term}`, {
         headers: {
@@ -33,53 +33,69 @@ function Main() {
       })
       .then(res => res.json())
       .then(data => {
-        let art = data.results.filter(result => result.type === 'artist');
-        return setArtistResults(art[0]);
+        let artists = data.results.filter(result => result.type === 'artist');
+        let masters = data.results.filter(result => result.type === 'master');
+        setIsLoading(false);
+        setArtists(artists[0]);
+        setMasters(masters);
       })
     } catch (err) {
       console.log(err);
     }
   }
 
-  // function searchAlbum(term) {
-  //   try {
-  //     fetch(`https://api.discogs.com/database/search?q=${term}`, {
-  //       headers: {
-  //         Authorization: `Discogs token=${apiKey}`
-  //       }
-  //     })
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       return data.results.map(result => {
-  //         return setAlbumResults(el => [...el, {
-  //           id: result.id,
-  //           genre: result.genre,
-  //           style: result.style,
-  //           title: result.title,
-  //           year: result.year,
-  //           img: result.cover_image
-  //         }])
-  //       })
-  //     })
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
-
-
   return (
     <div className="Main">
       <h1>Search</h1>
-      <Input onChange={handleTermChange} placeholder="Search Artist" />
-      <Button type="primary" onClick={() => handleArtistSearch()} >Go!</Button>
-        {artistResults
-          ?
-            <Link to={`/artists/${artistResults.id}`} >
-              <img src={artistResults.cover_image} alt="band"/>
-            </Link>
-          : 
-            ''
+      <div>
+        <Radio.Group 
+          defaultValue="artist" 
+          buttonStyle="solid" 
+          onChange={handleRadioValue}
+        >
+          <Radio.Button value="artist">Artist</Radio.Button>
+          <Radio.Button value="album">Album</Radio.Button>
+        </Radio.Group>
+        {radioValue === 'artist'
+          ? <div>
+              <Input 
+                onChange={handleTermChange} 
+                placeholder="Search by Artist" 
+                onPressEnter={() => handleSearch(searchTerm)}
+              />
+              <Button 
+                type="primary" 
+                onClick={() => handleSearch(searchTerm)}
+              >
+                Go!
+              </Button>
+                {artists.cover_image &&
+                  <Link to={`/artists/${artists.id}`} >
+                    <img src={artists.cover_image} alt="band"/>
+                  </Link>
+                }
+            </div>
+          : <div>
+            <Input 
+                onChange={handleTermChange} 
+                placeholder="Search by Album" 
+                onPressEnter={() => handleSearch(searchTerm)}
+              />
+              <Button 
+                type="primary" 
+                onClick={() => handleSearch(searchTerm)}
+              >
+                Go!
+              </Button>
+              <Albuns albuns={masters} />
+          </div>
         }
+      </div>
+      <div>
+        {isLoading &&
+          <Spin />
+        }
+      </div>
     </div>
   );
 }
