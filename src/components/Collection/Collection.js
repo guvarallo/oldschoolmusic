2d417b99c964890710d81637e7c77753e357e7c3
guvarallo <gustavo.varallo@live.com>
2020-10-11 20:19:45 -0300

    my collection styles

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './Collection.css'
 
 function Collection() {
-  return <h4>My Collection</h4>
+  return (
+    <div id="collectionId">
+      <h4>My Collection</h4>
+    </div>
+  ) 
 }
 
 export default Collection;
