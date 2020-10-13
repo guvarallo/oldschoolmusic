bfefe883380f2e0cc97ed14d036d34a1d85ecee8
guvarallo <gustavo.varallo@live.com>
2020-10-13 11:09:23 -0300

    fixed artist styles

--- src/components/Artists/Artists.js
+++ src/components/Artists/Artists.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { PlusCircleOutlined } from '@ant-design/icons';
+import { PlusCircleTwoTone } from '@ant-design/icons';
 
 import Releases from '../Releases/Releases';
 import './Artists.css';
 
 function Artists({ artist, onAdd }) {
+  const url = 'https://www.discogs.com';
+
   return (
     <div className="artist">
       {!!Object.keys(artist).length && 
       <>
         <h4>{artist.title}</h4>
-          <div className="container">
+        <div style={{ position: "relative" }}>
+          <a href={url + artist.uri} target="blank" className="artistContainer">
+            <img alt={artist.title} src={artist.img} className="image" />
+            <div className="middle">
+              <PlusCircleTwoTone />
+              <div style={{ 
+                color: "#1890ff", 
+                fontWeight: "bold" 
+              }}>
+                Click for more info at DiscoGS
+              </div>
+            </div>
+          </a>
+        </div>
+          {/* <div className="container">
             <a href={artist.uri} target="blank">
               <img src={artist.img} alt={artist.title} className="artistImg" />
             </a>
@@ -18,7 +34,7 @@ function Artists({ artist, onAdd }) {
               <PlusCircleOutlined />
               <div>Click for more info at DiscoGS</div>
             </div>
-          </div>
+          </div> */}
         <h4>{`Here are all the releases we found for ${artist.title}:`}</h4>
         <Releases 
           artistsUrl={artist.url} 
