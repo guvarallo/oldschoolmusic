463e18d4139c841cbf611fccb38a2a873a4cef5d
guvarallo <gustavo.varallo@live.com>
2020-10-15 08:49:35 -0300

    fixed discogs name

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,50 +1,59 @@
-import React from 'react';
-import { Row, Col, Card, Button } from 'antd';
-import { PlusCircleTwoTone } from '@ant-design/icons';
+import React from "react";
+import { Row, Col, Card, Button } from "antd";
+import { PlusCircleTwoTone } from "@ant-design/icons";
 
-import './Cards.css';
+import "./Cards.css";
 
 function Cards({ elements, onAdd }) {
-  const url = 'https://www.discogs.com';
+  const url = "https://www.discogs.com";
 
   return (
     <div>
       <Row gutter={16}>
-          {elements.map(element => {
-            return (
-              <Col key={element.id} span={4} className="elements">
-                <Card
-                  cover={
-                    <>
-                    <a href={`${url}/${element.type}/${element.id}`} target="blank">
-                      <div className="container">
-                        <img alt={element.title} src={element.img} className="image" />
-                        <div className="middle">
+        {elements.map(element => {
+          return (
+            <Col key={element.id} span={4} className='elements'>
+              <Card
+                cover={
+                  <>
+                    <a
+                      href={`${url}/${element.type}/${element.id}`}
+                      target='blank'
+                    >
+                      <div className='container'>
+                        <img
+                          alt={element.title}
+                          src={element.img}
+                          className='image'
+                        />
+                        <div className='middle'>
                           <PlusCircleTwoTone />
-                          <div style={{ 
-                            color: "#1890ff", 
-                            fontWeight: "bold" 
-                          }}>
-                            Click for more info at DiscoGS
+                          <div
+                            style={{
+                              color: "#1890ff",
+                              fontWeight: "bold",
+                            }}
+                          >
+                            Click for more info at Discogs
                           </div>
                         </div>
                       </div>
                     </a>
-                    </>
-                  }
-                >
-                  <p className="title">{element.title}</p>
-                  <p className="title">Year: {element.year}</p>
-                  <Button type="primary" onClick={() => onAdd(element)}>
-                    + Add to Collection
-                  </Button>
-                </Card>
-              </Col>
-            )
-          })}
+                  </>
+                }
+              >
+                <p className='title'>{element.title}</p>
+                <p className='title'>Year: {element.year}</p>
+                <Button type='primary' onClick={() => onAdd(element)}>
+                  + Add to Collection
+                </Button>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
     </div>
-  )
+  );
 }
 
 export default Cards;
--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -1,46 +1,54 @@
-import React from 'react';
-import { Col, Card, Button } from 'antd';
-import { PlusCircleTwoTone } from '@ant-design/icons';
+import React from "react";
+import { Col, Card, Button } from "antd";
+import { PlusCircleTwoTone } from "@ant-design/icons";
 
-import './Collection.css'
+import "./Collection.css";
 
 function Collection({ collection, onRemove }) {
-  const url = 'https://www.discogs.com';
+  const url = "https://www.discogs.com";
 
   return (
-    <div id="collectionId">
+    <div id='collectionId'>
       <h4>My Collection:</h4>
       {collection.map(el => {
         return (
-          <Col key={el.id} span={4} className="elements collections">
+          <Col key={el.id} span={4} className='elements collections'>
             <Card
               cover={
                 <>
-                <a href={`${url}/${el.type}/${el.id}`} target="blank">
-                  <div className="container">
-                    <img alt={el.title} src={el.img} className="image collectionImg" />
-                    <div className="middle">
-                      <PlusCircleTwoTone />
-                      <div style={{ 
-                        color: "#1890ff", 
-                        fontWeight: "bold" 
-                      }}>
-                        Click for more info at DiscoGS
+                  <a href={`${url}/${el.type}/${el.id}`} target='blank'>
+                    <div className='container'>
+                      <img
+                        alt={el.title}
+                        src={el.img}
+                        className='image collectionImg'
+                      />
+                      <div className='middle'>
+                        <PlusCircleTwoTone />
+                        <div
+                          style={{
+                            color: "#1890ff",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Click for more info at Discogs
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </a>
+                  </a>
                 </>
               }
             >
-              <p className="title">{el.title}</p>
-              <Button size="small" onClick={() => onRemove(el)}>Remove</Button>
+              <p className='title'>{el.title}</p>
+              <Button size='small' onClick={() => onRemove(el)}>
+                Remove
+              </Button>
             </Card>
           </Col>
-        )
+        );
       })}
     </div>
-  ) 
+  );
 }
 
 export default Collection;
